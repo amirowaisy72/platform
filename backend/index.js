a714063527735a77499336827f4469fc1baf9f91
amirowaisy72 <amirowaisy72@gmail.com>
2025-12-18 17:12:14 -0500

    update

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,16 @@ connectDB();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://platform-user-end.vercel.app",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 // =======================
 // 🔥 SSE CLIENT STORE
